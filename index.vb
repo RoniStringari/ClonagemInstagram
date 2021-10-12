ee172906b3aa0c8fe2fe0c151a558bfd625662d9
Roni <roni.stringari@gmail.com>
2021-10-11 22:35:48 -0300

    Clonagem Instagram

---
+++ index.vb
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Instagram</title>
+        
+
+    </head>
+    <body>
+        <div class="instagram-wrapper">
+            <div class="instagram-phone">
+                <img src="./insta-celular.png" alt="celular">
+            </div>
+            <div class="instagram-continue">
+                <div class="group">
+                    <img src="./instagram.png" class="instagram-logo" alt="instagram logo">
+                    <div class="profile-photo">
+                        <img src="./perfil.PNG" alt="perfil">
+                    </div>
+                    <a href="#" class="instagram-login">Continue como ronimstringari</a>
+                    <a href="#" class="instagram-logout">Remover conta</a>
+                </div>
+                <div class="group">
+                    <p class="not-account">Não é ronimstringari</p>
+                    <p class="not-account">
+                       <span class="link-blue">Alternar contas</span>
+                       ou
+                       <span class="link-blue">Inscreva-se</span> 
+                    </p>
+                </div>
+                <div class="get-the-app">
+                    <p class="get-app">Baixe o app</p>
+                    <div class="download">
+                        <a href="#" class="app-download"></a>
+                        <a href="#" class="app-download"></a>
+                    </div>
+                </div>
+            </div>        
+        </div>
+        
+        
+    </body>
+</html>
+
+
